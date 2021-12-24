ff25f6845fab2da28fc4c636a525b7190b8aa546
IAmrinderSingh <amrindersingh1703@gmail.com>
2021-12-24 07:38:20 +0530

    Modified MainActivity for binding Method Added calculating tip function to calculate tip

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tiptime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    fun calculateTip(){
+        val stringInViewField=binding.costOfService.text.toString()
+        val cost=stringInViewField.toDouble()
+        val selectId=binding.tipOptions.checkedRadioButtonId
+        val tipPercentage=when(selectId){
+            R.id.option_twenty_percent->0.20
+            R.id.option_eighteen_percent->0.18
+            else->0.15
+        }
+        var tip=tipPercentage*cost
+        val roundUp=binding.roundUpSwitch.isChecked
+        if (roundUp){
+            tip=kotlin.math.ceil(tip)
+        }
     }
 }
